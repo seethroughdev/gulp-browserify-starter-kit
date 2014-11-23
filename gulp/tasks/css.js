9a9372cad45981583a635057f1588b562c0eda2a
Adam L <github@adamin.net>
2014-11-22 23:45:01 -0800

    Improved css build process

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -2,16 +2,19 @@
 
 var gulp        = require('gulp')
 ,   $           = require('gulp-load-plugins')()
-,   browserSync = require('browser-sync')
-,   reload      = browserSync.reload;
+,   browserSync = require('browser-sync');
 
 var path         = require('../utils/paths')
 ,   handleErrors = require('../utils/handleErrors');
 
+var rubySassOpts = {
+  style: 'compressed',
+  'sourcemap=none': true
+};
 
 gulp.task('css:scss', function () {
   return gulp.src([ path.src.css + '*.scss' ])
-    .pipe($.rubySass())
+    .pipe($.rubySass(rubySassOpts))
     .on('error', handleErrors)
     .pipe($.autoprefixer('last 1 version', '> 1%', 'ie 8', 'ie 7'))
     .pipe($.csso())
