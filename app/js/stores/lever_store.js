b4833526c1a366e4f8dadabec7df0be981582275
Adam L <github@adamin.net>
2015-01-08 12:11:02 -0800

    Improved factory store

--- app/js/stores/lever_store.js
+++ app/js/stores/lever_store.js
@@ -52,7 +52,7 @@ leverStore = Reflux.createStore({
    * @return {Object}       New prototype obj of chart options
    */
   getChartInfo: function(lever, sub) {
-    return _.merge({}, ChartOpts[lever][sub], ChartProto);
+    return _.extend(Object.create(ChartOpts[lever][sub]), ChartProto);
   },
 
   /**
@@ -66,7 +66,7 @@ leverStore = Reflux.createStore({
       columns: _leverData[sub],
       unload: true
     };
-    return _.merge({}, o, ChartOpts[lever][sub].data);
+    return _.extend({}, o, ChartOpts[lever][sub].data);
   },
 
   /**
