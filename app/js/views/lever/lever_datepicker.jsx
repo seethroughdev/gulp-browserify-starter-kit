dd1a1e78bc6c71034a93a049ab51e0c6be471859
Adam L <github@adamin.net>
2015-01-13 01:08:58 -0800

    Added datepicker state

--- app/js/views/lever/lever_datepicker.jsx
+++ app/js/views/lever/lever_datepicker.jsx
@@ -1,18 +1,72 @@
 'use strict';
 
 var React = require('react/addons'),
+    $     = require('domtastic'),
     View;
 
 View = React.createClass({
 
+  getInitialState: function() {
+    return {
+      isActive: false,
+      activeText: 'Last 30 Days'
+    };
+  },
+
+  toggleSelector: function() {
+    this.setState({
+      isActive: !this.state.isActive
+    });
+  },
+
+  deactivateSelector: function() {
+    this.setState({
+      isActive: false
+    });
+  },
+
+  activateSelector: function(text, val) {
+    this.setState({
+      activeText: text
+    });
+    this.deactivateSelector();
+    console.log(val);
+  },
+
+  handleClick: function(e) {
+    e.preventDefault();
+    this.toggleSelector();
+  },
+
+  handleSelect: function(e) {
+    var $el = $(e.target);
+
+    this.activateSelector($el.text(), $el.attr('data-value'));
+
+    e.preventDefault();
+  },
+
+  handleMouseLeave: function(e) {
+    this.deactivateSelector();
+  },
+
   render: function() {
+
+    var cx = React.addons.classSet,
+        classes = cx({
+          'date-picker': true,
+          'filter__date': true,
+          'is-active': this.state.isActive
+        });
+
     return (
-      <div className="date-picker filter__date">
+      <div className={classes} onMouseLeave={this.handleMouseLeave}>
         <div className="date-picker__wrapper">
-          <div className="date-picker__current">
-            Last 14 Days
+          <div className="date-picker__current"
+            onClick={this.handleClick}>
+            {this.state.activeText}
           </div>
-          <ul className="date-picker__dropdown">
+          <ul className="date-picker__dropdown" onClick={this.handleSelect}>
             <li data-value="7c">This Week</li>
             <li data-value="30c">This Month</li>
             <li data-value="365c">This Year</li>
